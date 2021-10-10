75f10f4969e33a3f108bedf7ba0a470ac834f6a9
LukeL <gamesofscarpus@gmail.com>
2021-10-10 11:09:56 -0500

    yep renaming more

---
+++ App13_MathModule.py
@@ -0,0 +1,9 @@
+import math
+
+x = 2.9926
+y = -4.8
+
+print(x, y)
+print(str("ceiling of x is " + str(math.ceil(2.9))))
+print(str("round x is " + str(round(x))))
+print(str("absolute value of y is " + str(abs(y))))
---
+++ App14_IfElifCredit.py
@@ -0,0 +1,7 @@
+Hprice = 1000000
+creditgood = False
+
+if creditgood == True:
+    print("Down payment price is $" + str(float(Hprice) * 0.1))
+elif creditgood == False:
+    print("Down payment price is $" + str(float(Hprice) * 0.2))
